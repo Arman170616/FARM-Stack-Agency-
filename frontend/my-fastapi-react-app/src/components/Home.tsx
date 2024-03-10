6cf2e5e68e3f71277390bd32c49ba8b74a68c0b2
Arman170616 <armanice09@gmail.com>
2024-03-10 19:02:14 +0600

    Home page

--- frontend/my-fastapi-react-app/src/components/Home.tsx
+++ frontend/my-fastapi-react-app/src/components/Home.tsx
@@ -1,11 +1,23 @@
-// Home.tsx
 import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <p>Welcome to our website!</p>
-    </div>
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-orange-500">
+      <div className="container px-4 md:px-6">
+        <div className="flex flex-col items-center space-y-4 text-center">
+          <div className="space-y-2">
+            <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
+              The future of frontend development
+            </h1>
+            <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-700">
+              The only platform you need to build, deploy, and scale your web apps. Sign up to get notified when we
+              launch.
+            </p>
+          </div>
+          
+        </div>
+      </div>
+    </section>
   );
 }
 
